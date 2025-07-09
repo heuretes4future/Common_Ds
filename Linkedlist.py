4d3a23d9170e440d3457e9c16045526d876cd432
Oscar <lundinoscar4@gmail.com>
2025-07-09 18:07:45 +0200

    added function for double llist

--- Linkedlist.py
+++ Linkedlist.py
@@ -1,5 +1,18 @@
 import random
 
+
+''' match llist_type:
+            case "single":
+                self.head = new_node          
+                self.root = new_node
+                self.size += 1 
+                return new_node
+            case "double":
+
+'''
+         
+
+
 class linkedlist(object):
     def __init__(self, r = None):
         self.root = r 
@@ -11,14 +24,25 @@ class linkedlist(object):
     def add(self, d):
         new_node = Node(d, self.root)
         self.root = new_node
+        self.size +=1     
+        return new_node   
+
+   
+    def double_link(self,d):
+        new_node = Node(d, self.root)
+        self.root = new_node
+        old_node = new_node.get_next()
+        if old_node is None:
+            return new_node
+        old_node.prev_node = new_node
         self.size += 1 
         return new_node
-   
 
 class Node(object):
-    def __init__(self, d, n = None):
+    def __init__(self, d, n = None,t = None):
         self.data = d
         self.next_node = n
+        self.prev_node = t
 
     def get_next(self):
         return self.next_node
@@ -31,8 +55,9 @@ class Node(object):
     
     def set_data(self, d):
         self.data = d
-
+   
 def main():
+    # Preview
     llist = linkedlist()
     Amount_of_nodes = input("Amount of nodes to add -> ")
 
@@ -40,7 +65,7 @@ def main():
         Amount_of_nodes = int(Amount_of_nodes)
         for curr_loop in range (Amount_of_nodes):
             current_node_value = input(f'{curr_loop} Add a value > ')
-            currentnode = llist.add(current_node_value)
+            currentnode = llist.double_link(current_node_value)
 
 
     node_value_list = []
@@ -48,5 +73,7 @@ def main():
         node_value_list.append(currentnode.get_data())
         currentnode = currentnode.next_node
     print(f'->'.join(node_value_list))
+
+
 if __name__ == '__main__':
     main()
