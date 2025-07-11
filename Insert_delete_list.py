4995b10ff052cc501d0871b6b37eee97cbedc1f4
Oscar Lundin <lundinoscar4@gmail.com>
2025-07-11 17:10:46 +0200

    finishing

--- Insert_delete_list.py
+++ Insert_delete_list.py
@@ -1,35 +1,35 @@
-import math 
+import random 
 
 def insertion(insertnumber, prim_list):
-
+    #an easy insert type function
     if insertnumber in prim_list:
         next_pos = 0
         working_position = prim_list.index(insertnumber)
-        for current_position in prim_list[:working_position]:
+        for _ in prim_list:
+            curr = prim_list[-1-next_pos]
+            if curr == insertnumber:
+                prim_list[working_position] = insertnumber
+                return insertnumber
+            prim_list.append(curr)
             next_pos +=1
-            prim_list.append(prim_list[-1-next_pos])
-        prim_list.insert(working_position,insertnumber)
 
-    [prim_list[len(prim_list) - position].append(prim_list[len(prim_list)]) for position in range(len(prim_list)) if prim_list[position] == insertnumber]
-    return insertnumber
+#def deletion(deletenumber, prim_list):
 
-def deletion(deletenumber, prim_list):
-
-    return deletenumber
+#    return deletenumber
 
 
 def main():
-    prim_list = [math.random(i) for i in range(math.random)]
+    list_size = random.randint(1, 100) 
+    prim_list = [random.randint(1, 100) for _ in range(list_size)]
     print(f"Randomized list: {prim_list}")
-    input("Chose by typing either insert, delete" )
     while True:
-        UserInput = input("input the number you want to manipulate > ")
-        if UserInput.isdigit and UserInput in UserInput:
+        UserInput = input("which position do you want to insert in > ")
+        if UserInput.isdigit():
             UserInput = int(UserInput)
             try:
-                removed = deletion(UserInput, prim_list)
+           #     removed = deletion(UserInput, prim_list)
                 inserted = insertion(UserInput,prim_list)
-                input(f"value that has been manipulated: {removed}")
+           #     input(f"value that has been manipulated: {removed}")
             except ValueError:
                 print("Add a valid number")
 
@@ -37,3 +37,5 @@ def main():
          print("Enter a valid number that exist in the list")
 
 
+if __name__ == "__main__":
+    main()
