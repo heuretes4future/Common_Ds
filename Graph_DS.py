8a61e7a54035e9afa9dfbb22687f1f9a326f2e6f
Oscar <lundinoscar4@gmail.com>
2025-07-13 23:59:30 +0200

    structure set up, currently working on binary class/functions

--- Graph_DS.py
+++ Graph_DS.py
@@ -1,5 +1,5 @@
 #A basic graph 
-
+from collections import deque
 
 class graph:
     def __init__(self):
@@ -15,7 +15,7 @@ class node:
         self.left = None
         self.right = None
 
-
+#binary tree with structured where root is always middle value. 
 class binary_tree():
     def __init__(self):
         self.root = None
@@ -23,13 +23,20 @@ class binary_tree():
 
     def create_tree(self,input_list):
         input_list.sort()
+        left_list = []
+        right_list = []
         middle = len(input_list)//2
-        
-        self.root = input_list.pop()
-        self.root = node(value)
-        for value in input_list:
-            current_node = node(value)
-
+        for _ in range(middle):
+             if len(input_list) == 1:
+                self.root = node(input_list.pop())
+             left_list.append(input_list.pop())
+             right_list.append(input_list.deque.popleft())
+        if self.root == None:
+            self.root = node((left_list[-1] + left_list[-1]) // 2)
+        for _ in range(middle):
+            #From the root left and right should be connected to right_list[-1] and left_list[-1] since
+            #that will be the highest value, after that make sure all values are properly linked to eachother. 
+            pass
 
 
 def main():
@@ -37,7 +44,7 @@ def main():
     bin = binary_tree()
     #the binary tree takes where values split from smallest to biggest 
 
-    num_list = [15,10,5,0]
+    num_list = deque([15,10,5,0])
     graphing.populate_treee(len(num_list))
 
 if __name__ == "__main__":
